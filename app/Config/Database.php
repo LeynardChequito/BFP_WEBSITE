78f57e1be7633e9529252f6df4ce6a2bfc9103cb
LeynardChequito <144776361+LeynardChequito@users.noreply.github.com>
2024-04-14 20:02:52 +0800

    Database_hosting

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,9 +27,9 @@ class Database extends Config
     public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
-        'username'     => 'root',
-        'password'     => '',
-        'database'     => 'bfp',
+        'username'     => 'u262559041_bfpcalapancity',
+        'password'     => 'Bfpcalapan@2024',
+        'database'     => 'u262559041_bfp',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
