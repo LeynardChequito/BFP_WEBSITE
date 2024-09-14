04799b5462dc4ae97e923807ba25dfb847944874
LeynardChequito <144776361+LeynardChequito@users.noreply.github.com>
2024-09-14 23:15:09 +0800

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,6 +1,7 @@
-importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js')
-importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js')
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
 
+// Initialize Firebase in the service worker with the same configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAiXnOQoNLOxLWEAw5h5JOTJ5Ad8Pcl6R8",
   authDomain: "pushnotifbfp.firebaseapp.com",
@@ -12,6 +13,7 @@ const firebaseConfig = {
 };
 
 firebase.initializeApp(firebaseConfig);
+
 const messaging = firebase.messaging();
 
 // Background message handler
