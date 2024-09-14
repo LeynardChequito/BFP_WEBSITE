aa1159a891d8552fa1c8968d3d17fcbc4e066788
LeynardChequito <144776361+LeynardChequito@users.noreply.github.com>
2024-09-15 01:25:08 +0800

    Update adminheader.php

--- app/Views/ACOMPONENTS/adminheader.php
+++ app/Views/ACOMPONENTS/adminheader.php
@@ -315,7 +315,14 @@
                 });
             }
         }
-
+        if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('/firebase-messaging-sw.js')
+            .then(function(registration) {
+                console.log('Service Worker registered with scope:', registration.scope);
+            }).catch(function(err) {
+                console.log('Service Worker registration failed:', err);
+            });
+    }
         // Open modal on notification click
         $(".notification-dropdown").on("click", function() {
             modal.style.display = "block";
