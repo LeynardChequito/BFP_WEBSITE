6abee088a84ef1e8c2245a84bf6fc7abc12f05b3
LeynardChequito <144776361+LeynardChequito@users.noreply.github.com>
2024-09-15 02:19:22 +0800

    Update adminheader.php

--- app/Views/ACOMPONENTS/adminheader.php
+++ app/Views/ACOMPONENTS/adminheader.php
@@ -319,7 +319,7 @@ const messaging = firebase.messaging();
             }
 
             if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/firebase-messaging-sw.js')
+    navigator.serviceWorker.register('/public/firebase-messaging-sw.js')
         .then(function (registration) {
             console.log('Service Worker registered with scope:', registration.scope);
         }).catch(function (err) {
