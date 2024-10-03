c5bbf671b40eb5e2ea0eb1887586e4efd17b31c8
Leynard <cjohnleynard399@gmail.com>
2024-10-04 02:27:22 +0800

    Update MapScript.php

--- app/Views/EMERGENCYCALL/MapScript.php
+++ app/Views/EMERGENCYCALL/MapScript.php
@@ -88,7 +88,7 @@ const baseUrl = "<?= base_url() ?>";
         newReportsList.appendChild(listItem);
 
         // Add the user marker to the map
-        const userMarker = L.marker([latitude, longitude], { icon: userIcon }).addTo(map);
+        const userMarker = L.marker([latitude, longitude], { icon: userMarker }).addTo(map);
         userMarker.bindPopup(`User in Need: ${fullName}`).openPopup(); // Popup with user's name
     });
 }
